cd848d4a5d23a47dffceabedf78bbc0af6403b3e
佐藤尚彦 <otopiko@satounaohiko-no-MacBook-Air.local>
2016-04-06 00:06:06 +0900

    second commit

---
+++ polls/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+
+from .models import Question,Choice
+
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3 
+
+
+class QuestionAdmin(admin.ModelAdmin):
+        fieldsets = [
+	            (None,               {'fields' : ['question_text']}),
+	            ('Date information', {'fields' : ['pub_date']}),
+                     ]
+	inlines = [ChoiceInline]
+	list_filter = ['pub_date']
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	search_fields = ['quetion_text']
+
+admin.site.register(Question,QuestionAdmin)
+
+# Register your models here.
---
+++ polls/tests.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+import datetime
+from django.utils import timezone
+from django.test import TestCase
+from .models import Question
+
+def create_question(question_text, days):
+	    time = timezone.now() + datetime.timedelta(days=days)
+	    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionViewTests(TestCase):            
+	def test_index_view_with_no_questions(self):
+	    response = self.client.get(reverse('polls:index'))
+	    self.assertEqual(response.status_code, 200)
+	    self.assertContains(response, "No polls are available.")
+	    self.assertQuerysetEqual(response.context['latest_question_list'],[])
+
+	def test_index_view_with_a_past_question(self):
+	    create_question(question_text="Future question.", days=30)
+	    response = self.client.get(reverse('polls:index'))
+	    self.assertQuerysetEqual(response.context['latest_question_list'],['<Question: Past question.>'])
+
+	def test_index_view_with_future_question(self):
+	    create_question(question_text="Future question.", days=30)
+	    response = self.client.get(reverse('polls:index'))
+	    self.assertContains(response, "No polls are available.", status_code=200)
+	    self.assertQuerysetEqual(response.context['latest_question_list'],[])
+
+class QuestionIndexDetailTests(TestCase):
+	def test_detail_view_with_a_future_question(self):
+	    future_question = create_question(question_text='Future question.', days=5)
+	    response = self.client.get(reverse('polls:detail',args=(future_question.id,)))
+	    self.assertEqual(response.status_code, 404)
+
+	def test_detail_view_with_a_past_question(self):
+	    past_question = create_question(question_text='Past Question.', days=-5)
+	    response = self.client.get(reverse('polls:detail',args=(past_question.id,)))
+	    self.assertContains(response, past_question.question_text, status_code=200)
+
+# Create your tests here.
+
